cd0a95a70b20a5163d103939fdab635f761e5665
Emerson Shigueo Sugimoto <emerson.sugimoto@serpro.gov.br>
2025-02-12 13:24:02 -0300

    upload imagem chat principal

--- src/app/components/chat-ollama-page/chat-ollama-page.component.ts
+++ src/app/components/chat-ollama-page/chat-ollama-page.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ChatResponse, GenerateResponse, ModelResponse } from 'ollama';
+import { ModelResponse } from 'ollama';
 import { delay } from 'rxjs';
 import { Configuracoes, TipoMensagem, UnifiedChatResponse } from '../../model/modelos';
 import { ConfiguracoesService } from '../../services/configuracoes.service';
@@ -40,6 +40,7 @@ export class ChatOllamaPageComponent {
   protected userPrompt: string = '';
   protected modelos: ModelResponse[] | undefined;
   protected isServerOnline = signal(false);
+  private images: string[] = [];
 
 
   constructor(private configuracoesService: ConfiguracoesService, private ollamaChatService: OllamaChatService) {
@@ -59,7 +60,8 @@ export class ChatOllamaPageComponent {
     });
 
     // Teste
-    this.userPrompt = 'Crie uma classe em Java para tratar caracteres da String valorTeste que não estejam no range entre A e Z, use regex';
+    //this.userPrompt = 'Crie uma classe em Java para tratar caracteres da String valorTeste que não estejam no range entre A e Z, use regex';
+    this.userPrompt = 'Descreva a imagem';
   }
 
   //#region btns
@@ -86,53 +88,44 @@ export class ChatOllamaPageComponent {
     this.toggleForm(false);
   }
 
-  btnEnviarMensagem() {
-    const test = false;
+  public btnEnviarMensagem(): void {
+    if (!this.userPrompt || this.userPrompt.trim().length <= 0) { this.mensagemComponente?.show('Informe a mensagem', TipoMensagem.ALERTA, 700); this.scrollMsgAppMensagem(); return; }
+    if (!this.selectedModel || this.selectedModel.trim().length <= 0) { this.mensagemComponente?.show('Selecione o modelo', TipoMensagem.ALERTA, 700); this.scrollMsgAppMensagem(); return; }
 
-    if (!test) {
-      if (!this.userPrompt || this.userPrompt.trim().length <= 0) { this.mensagemComponente?.show('Informe a mensagem', TipoMensagem.ALERTA, 700); this.scrollMsgAppMensagem(); return; }
-      if (!this.selectedModel || this.selectedModel.trim().length <= 0) { this.mensagemComponente?.show('Selecione o modelo', TipoMensagem.ALERTA, 700); this.scrollMsgAppMensagem(); return; }
+    let temperatura = this.configuracoes.configuracoes!.temperatura;
+    if (!temperatura || temperatura < 0) { temperatura = 0.7; this.configuracoes.configuracoes!.temperatura = temperatura; }
 
-      let temperatura = this.configuracoes.configuracoes!.temperatura;
-      if (!temperatura || temperatura < 0) { temperatura = 0.7; this.configuracoes.configuracoes!.temperatura = temperatura; }
+    this.toggleForm(true);
+    this.updateChatTxt(this.userPrompt);
+    this.loadingChat();
 
-      this.toggleForm(true);
-      this.updateChatTxt(this.userPrompt);
-      this.loadingChat();
+    let index = -1;
 
-      let index = -1;
-
-      this.ollamaChatService.consultaOllama(
-        this.configuracoes.configuracoes?.modo,
-        this.userPrompt, this.selectedModel, temperatura, this.appchatdisplay?.getHistorico() ?? []
-      ).subscribe({
-        next: (res) => {
-          if (index < 0) {
-            index = this.updateChat(res);
-          } else {
-            this.updateChatMessage(index, res);
-          }
-        },
-        error: (err) => {
-          this.updateChatTxt(err, true);
-          this.mensagemComponente?.show('Erro na comunicação com o Ollama', TipoMensagem.ERRO, 700);
-          this.scrollMsgAppMensagem();
-        },
-        complete: () => {
-          this.mensagensOverlayComponent?.hide();
-          this.appchatdisplay?.scrollToLastMessage();
-          this.loadingChat(false);
-          this.toggleForm(false);
+    this.ollamaChatService.consultaOllama(
+      this.configuracoes.configuracoes?.modo,
+      this.userPrompt, this.selectedModel, temperatura,
+      this.appchatdisplay?.getHistorico() ?? [],
+      this.images
+    ).subscribe({
+      next: (res) => {
+        if (index < 0) {
+          index = this.updateChat(res);
+        } else {
+          this.updateChatMessage(index, res);
         }
-      });
-    } else {
-      this.loadingChat();
-      setTimeout(() => {
-        this.testeResposta();
+      },
+      error: (err) => {
+        this.updateChatTxt(err, true);
+        this.mensagemComponente?.show('Erro na comunicação com o Ollama', TipoMensagem.ERRO, 700);
+        this.scrollMsgAppMensagem();
+      },
+      complete: () => {
+        this.mensagensOverlayComponent?.hide();
+        this.appchatdisplay?.scrollToLastMessage();
         this.loadingChat(false);
-      }, 1200);
-    }
-
+        this.toggleForm(false);
+      }
+    });
   }
   //#endregion
 
@@ -162,59 +155,38 @@ export class ChatOllamaPageComponent {
     this.appchatdisplay?.updateMessage(index, response);
   }
 
-  private testeResposta() {
-    this.updateChatTxt('Testando');
+  //#region image upload
+  public onFileSelected(event: any): void {
+    this.images = [];
 
-    const resposta = `
-    <p>Aqui está um exemplo simples em Java para criar uma classe que remove todos os caracteres que não são 'A' ou 'B':</p>
-    <pre><code class="java language-java">public class RemoveNonAB {
-        // Método para remover caracteres não desejados e retornar String com apenas 'A's ou 'B's
-        public static String cleanString(String input) {
-            if (input == null || input.isEmpty()) {
-                return "";
-            } else {
-                StringBuilder output = new StringBuilder();
-                for (char c : input.toCharArray()) {
-                    // Verifica se o caractere é 'A' ou 'B', e adiciona ao novo StringBuilder
-                    if (c == 'A' || c == 'B') {
-                        output.append(c);
-                    }
-                }
-                return output.toString();
-            }
-        }
+    const files: FileList = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
 
-        // Método principal para testar a classe RemoveNonAB
-        public static void main(String[] args) {
-            String input = "AABBCCDDBBAA";
-            System.out.println("Original string: " + input);
+      reader.onload = (e: any) => {
+        const arrayBuffer = e.target.result;
+        const uint8Array = new Uint8Array(arrayBuffer);
+        const base64String = this.arrayBufferToBase64(uint8Array);
 
-            // Chama o método cleanString e exibe resultado
-            String cleanedString = cleanString(input);
-            System.out.println("Cleaned string: " + cleanedString);
-        }
+        // Adiciona Base64 string ao array de imagens
+        this.images.push(base64String);
+      };
+
+      reader.readAsArrayBuffer(file);
     }
-    </code>
-    </pre>
-    <pre><code class="css language-css">body {
-        font-family: Arial, sans-serif;
-        background-color: #f0f0f0;
-    }
-
-    h1 {
-        color: #333;
-    }
-
-    p {
-        line-height: 1.6;
-    }
-    </code></pre>
-    <p>Neste código, a classe <code>RemoveNonAB</code> possui um único método estático chamado <code>cleanString</code>. O método recebe uma String como entrada e retorna uma nova String que contém apenas caracteres 'A' ou 'B'. Ele itera sobre cada caractere na string original, verificando se ele é válido (ou seja, um 'A' ou um 'B'). Se o caractere for válido, ele será adicionado ao <code>StringBuilder</code> que armazena a saída.</p>
-    <p>O método principal chama este método e exibe tanto a string original quanto a string limpa resultante.</p>
-    `;
-
-    this.updateChatTxt(resposta, true);
   }
+
+  private arrayBufferToBase64(buffer: Uint8Array): string {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  }
+  //#endregion
 
   private toggleForm(desabilitar: boolean = true) {
     let txtUserPrompt = document.getElementById('txtUserPrompt') as HTMLInputElement;
@@ -222,6 +194,8 @@ export class ChatOllamaPageComponent {
     let btnLimpar = document.getElementById('btnLimpar') as HTMLButtonElement;
     let btnEnviar = document.getElementById('btnEnviar') as HTMLButtonElement;
     let temperature = document.getElementById('temperature') as HTMLInputElement;
+    let fileInputImage = document.getElementById('fileInputImage') as HTMLInputElement;
+    let btnEnviarImagem = document.getElementById('btnEnviarImagem') as HTMLInputElement;
 
     if (!!txtUserPrompt) {
       txtUserPrompt.focus();
@@ -231,7 +205,8 @@ export class ChatOllamaPageComponent {
     if (!!btnLimpar) { btnLimpar.disabled = desabilitar; }
     if (!!btnEnviar) { btnEnviar.disabled = desabilitar; }
     if (!!temperature) { temperature.disabled = desabilitar; }
-
+    if (!!fileInputImage) { fileInputImage.disabled = desabilitar; }
+    if (!!btnEnviarImagem) { btnEnviarImagem.disabled = desabilitar; }
   }
 
   private scrollMsgAppMensagem(timeout = 35) {
